14e966a735732a35c497c89ee62b5b4e88b560b9
maksym.palamariuk <maksym.palamariuk@cleverbridge.com>
2024-06-10 01:23:49 +0300

    Airflow: Replace Minio with S3

--- services/airflow/dags/upload_data.py
+++ services/airflow/dags/upload_data.py
@@ -4,32 +4,33 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.dummy_operator import DummyOperator
 from datetime import datetime
-from minio import Minio
-from minio.error import S3Error
 
+import boto3
+from botocore.exceptions import NoCredentialsError
 
-def upload_to_minio(file_path, bucket_name, object_name):
-    logging.info("Initializing Minio client...")
-    client = Minio(
-        'minio:9000',
-        access_key='minio',
-        secret_key='minio123',
-        secure=False
-    )
+def upload_to_s3(file_path, bucket_name, object_name):
+    logging.info("Initializing S3 client...")
+    s3_client = boto3.client('s3',
+                             aws_access_key_id='...',
+                             aws_secret_access_key='...',
+                             region_name='eu-central-1')
     try:
         logging.info(f"Uploading {file_path} to bucket {bucket_name} as {object_name}...")
-        client.fput_object(bucket_name, object_name, file_path)
+        s3_client.upload_file(file_path, bucket_name, object_name)
         logging.info(f"File '{file_path}' uploaded to bucket '{bucket_name}' as '{object_name}'.")
-    except S3Error as e:
+    except NoCredentialsError:
+        logging.error("Credentials not available.")
+        raise
+    except Exception as e:
         logging.error(f"Error occurred: {e}")
         raise
 
 
 def upload_task():
     file_path = '/opt/airflow/data/train.json'
-    bucket_name = 'mybucket'
+    bucket_name = 'mlops-news-trends'
     object_name = 'data/train.json'
-    upload_to_minio(file_path, bucket_name, object_name)
+    upload_to_s3(file_path, bucket_name, object_name)
 
 
 default_args = {
@@ -38,7 +39,7 @@ default_args = {
     'retries': 1,
 }
 
-with DAG('upload_to_minio_dag',
+with DAG('upload_train_data_to_s3_dag',
          default_args=default_args,
          schedule_interval='@daily',
          catchup=False) as dag:
@@ -46,11 +47,11 @@ with DAG('upload_to_minio_dag',
 
     end = DummyOperator(task_id='end')
 
-    upload_to_minio_task = PythonOperator(
-        task_id='upload_to_minio_task',
+    upload_to_s3_task = PythonOperator(
+        task_id='upload_to_s3_task',
         python_callable=upload_task
     )
 
-    start >> upload_to_minio_task >> end
+    start >> upload_to_s3_task >> end
 
 
